b307a61c743d10b396f4ff87a85eae682a6ef7ee
laxmip21 <92743748+laxmip21@users.noreply.github.com>
2022-01-18 13:01:21 -0500

    added files

---
+++ groupactivity/src/main/java/com/example/groupactivity/GroupactivityApplication.java
@@ -0,0 +1,13 @@
+package com.example.groupactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupactivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupactivityApplication.class, args);
+	}
+
+}
